f08802b334085556be3fe64bd33a8f908cc7c3c0
Cong Nguyen <congnguyenit@hotmail.com>
2022-05-15 03:42:57 +0700

    init

---
+++ src/components/navigation.jsx
@@ -0,0 +1,68 @@
+export const Navigation = (props) => {
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>Toggle navigation</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+          <a className='navbar-brand page-scroll' href='#page-top'>
+           TheIO.VN
+          </a>{' '}
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <a href='#features' className='page-scroll'>
+                Tính Năng
+              </a>
+            </li>
+            <li>
+              <a href='#about' className='page-scroll'>
+                Giới Thiệu
+              </a>
+            </li>
+            <li>
+              <a href='#services' className='page-scroll'>
+                Dịch Vụ
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio' className='page-scroll'>
+                Bộ Sưu Tập
+              </a>
+            </li>
+            <li>
+              <a href='#testimonials' className='page-scroll'>
+                Khách Hàng
+              </a>
+            </li>
+            <li>
+              <a href='#team' className='page-scroll'>
+                Đội Ngũ
+              </a>
+            </li>
+            <li>
+              <a href='#contact' className='page-scroll'>
+                Liên Hệ
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
