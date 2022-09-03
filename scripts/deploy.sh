ec83329de1b75a51c530f1deb212bcf5abeaefa9
Cris <cong.ntdc@utotech.vn>
2022-09-03 16:15:52 +0700

    run docker ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 bash --login <<EOF
 sudo docker -v
 sudo docker ps -q --filter ancestor="theio.vn" | xargs -r docker stop
-sudo docker run --rm -p 80:80 --name theio.vn congnguyenit/theio.vn:latest
+sudo docker run --rm -d -p 80:80 --name theio.vn congnguyenit/theio.vn:latest
 EOF
