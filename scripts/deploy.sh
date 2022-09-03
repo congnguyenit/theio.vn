c799e6d678364f30be04fac754ade5fb0552be5a
Cris <cong.ntdc@utotech.vn>
2022-09-03 15:32:41 +0700

    run docker ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 <<EOF
 docker -v
 docker ps -q --filter ancestor="theio.vn" | xargs -r docker stop
-docker run --rm -p 80:80 --name theio.vn $1
+docker run --rm -p 80:80 --name theio.vn congnguyenit/theio.vn:latest
 EOF
